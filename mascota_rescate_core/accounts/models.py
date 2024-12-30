681c0d565f2804e87a88d2bfa13d48d7f846e9d3
JuliAlvarez-P <julieta2000facultad@gmail.com>
2024-12-30 00:00:06 -0300

    Update CustomUser model with explicit username validator

--- mascota_rescate_core/accounts/models.py
+++ mascota_rescate_core/accounts/models.py
@@ -1,9 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 class CustomUser(AbstractUser):
     """Custom user model with additional fields."""
+    
+    username_validator = UnicodeUsernameValidator()
+    
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
     
     # Additional fields
     profile_image = models.ImageField(
